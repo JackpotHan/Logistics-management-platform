1e0f9e2e1838aaff5e42d9755b5a2d7f53b33709
hanjt <hjt950722@163.com>
2018-08-02 10:36:41 +0800

    第一次提交

---
+++ lmp-base/src/main/java/com/xiaohan/base/BaseObject.java
@@ -0,0 +1,17 @@
+package com.xiaohan.base;
+
+
+import java.io.Serializable;
+
+/**
+ * @Author: Hanjt
+ * @Date: 2018/8/2 10:33
+ * @Description:
+ */
+public class BaseObject implements Serializable {
+
+//    @Override
+//    public String toString() {
+//        return JSON.toJSONString(this);
+//    }
+}
