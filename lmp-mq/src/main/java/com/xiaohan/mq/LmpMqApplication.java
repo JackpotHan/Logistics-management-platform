a182949c1111d4c11b061c5683d720a90c85cc19
hanjt <hjt950722@163.com>
2018-08-02 14:25:47 +0800

    初期框架搭建

---
+++ lmp-mq/src/main/java/com/xiaohan/mq/LmpMqApplication.java
@@ -0,0 +1,12 @@
+package com.xiaohan.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmpMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmpMqApplication.class, args);
+    }
+}
---
+++ lmp-mq/src/test/java/com/xiaohan/mq/LmpMqApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xiaohan.mq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LmpMqApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
