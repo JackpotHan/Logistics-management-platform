9a3b6121edd6fe347851dd8124cde069dd1ffbbc
hanjt <hjt950722@163.com>
2018-08-06 10:28:59 +0800

    服务注册消费中心eureka

---
+++ lmp-eureka/src/main/java/com/xiaohan/eureka/LmpEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.xiaohan.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmpEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmpEurekaApplication.class, args);
+    }
+}
