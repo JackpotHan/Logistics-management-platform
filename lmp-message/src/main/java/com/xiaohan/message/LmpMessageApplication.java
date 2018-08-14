913e939db469e4a411f3be3398b3df77957e9e8e
hanjt <hjt950722@163.com>
2018-08-14 17:30:08 +0800

    Ali大雨短信模块

---
+++ lmp-message/src/main/java/com/xiaohan/message/LmpMessageApplication.java
@@ -0,0 +1,12 @@
+package com.xiaohan.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmpMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmpMessageApplication.class, args);
+    }
+}
